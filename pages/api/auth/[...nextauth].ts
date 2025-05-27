5fc71455e9c9f7274b54a6c8158dbf8bb4b53c2d
Raphael Copony <rawphael@users.noreply.github.com>
2025-05-27 22:11:38 +0200

    fix

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -90,8 +90,7 @@ export const authOptions: NextAuthOptions = {
         sameSite: "lax",
         path: "/",
         // When working on localhost, the cookie domain must be omitted entirely (https://stackoverflow.com/a/1188145)
-        domain: VERCEL_DEPLOYMENT ? `.${process.env.DOMAIN}` : undefined,
-        secure: VERCEL_DEPLOYMENT,
+        domain: VERCEL_DEPLOYMENT ? (process.env.NEXT_PUBLIC_BASE_URL ? `.${new URL(process.env.NEXT_PUBLIC_BASE_URL).hostname}` : undefined) : undefined,        secure: VERCEL_DEPLOYMENT,
       },
     },
   },
